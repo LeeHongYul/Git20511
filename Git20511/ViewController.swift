cf958e3ceb05fe6abfb89e7719183408e6fe775a
hongryul <ryullee0911@gmail.com>
2023-05-11 14:51:54 +0900

    C8

--- Git20511/ViewController.swift
+++ Git20511/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var a = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -27,7 +29,7 @@ class ViewController: UIViewController {
     }
 
     deinit {
-        
+
     }
 }
 
